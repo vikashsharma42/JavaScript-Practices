8ad1cfb065810cb253668e939db45455bc72486a
Vikash Kumar <109655770+vikashsharma42@users.noreply.github.com>
2024-07-11 06:59:16 +0000

    practise on data conersion

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,71 @@
+let score =9399
+
+console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+
+let num = "3578"
+
+console.log(typeof num)
+let valueofNumber = Number(num)
+console.log(valueofNumber)
+console.log(typeof valueofNumber)
+
+let num2 = "486ssc"
+console.log(num2)
+let valueOfnum2 = Number(num2)
+console.log(valueOfnum2) //It gives NAN.
+console.log(typeof valueOfnum2)
+
+
+let phoneNumber = null
+console.log(phoneNumber)
+console.log(typeof phoneNumber)
+let dummyphoneNumber = Number(phoneNumber)
+console.log(dummyphoneNumber)
+
+let zipCode = undefined
+console.log(zipCode)
+console.log(typeof zipCode)
+let dummyZipCode = Number(zipCode)
+console.log(dummyZipCode)
+console.log(typeof dummyZipCode)
+
+
+let isActive = false
+console.log(isActive) 
+console.log(typeof isActive)
+let dummyIsActive = Number(isActive)
+console.log(dummyIsActive)
+console.log(typeof dummyIsActive)
+
+// 33 => 33
+// "33asc" => NaN
+// true => 1; false =>0 
+
+let InActive = 1
+console.log(InActive)
+let convertedInActive = Boolean(InActive)
+console.log(convertedInActive)
+
+
+let isActiveStatus = ""
+console.log(isActiveStatus)
+let convertedIsActiveStatus = Boolean(isActiveStatus)
+console.log(convertedIsActiveStatus)
+
+let activeStatus = "Vikash"
+console.log(activeStatus)
+let convertedActiveStatus = Boolean(activeStatus)
+console.log(convertedActiveStatus)
+
+// 1 => true; 0 =>false
+// "" => false
+// "vikash" => true
+
+
+let studentId =10089
+console.log(studentId)
+let convertedStringId = String(studentId)
+console.log(convertedStringId)
+console.log(typeof convertedStringId)
