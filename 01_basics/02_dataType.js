1c612e41543904b11e074d7a7ecbfe73943cc524
Vikash Kumar <109655770+vikashsharma42@users.noreply.github.com>
2024-07-11 06:13:26 +0000

    Some Details disscussion about datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; //Traeat all JS code as newer version
+
+//alert("Hello Developer")
+/**
+ * We are using node js not browser.
+ * alert works only in browser.
+ * for using alert in node js we have to modify the code
+ */
+
+let name = "Vikash Kumar"  //string
+let age = 21  //age
+let isLoggedIn = false  //boolean
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null  // It is a standalone value
+//undifined => It means varibale is created but value is not assigneed.
+//symbol  => it is a unique value
+
+//boject
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaScript-Practices
+
+We have practics java script for industry lavel
